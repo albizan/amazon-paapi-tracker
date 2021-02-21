b9c0af7755e0d6b50a1a7e75acd365a671a1d87b
Alberto Zanotti <albizan@hotmail.it>
2021-02-21 16:36:17 +0100

    create queue and pass it to tasks

--- src/TaskManager/TaskManager.ts
+++ src/TaskManager/TaskManager.ts
@@ -3,55 +3,59 @@
 
 import * as config from "config";
 import { ToadScheduler } from "toad-scheduler";
-import Task from "../Task";
+import { Queue } from "bullmq";
+import Task from "../PaapiTask";
 import Paapi from "../Paapi";
 import PaapiCredentials from "../PaapiCredentials";
 
 export default class SchedulerManager {
   private paapiCredentialsAsList: PaapiCredentials[];
+  private paapiScheduler: ToadScheduler;
   private tasks: Task[];
 
   constructor() {
     this.paapiCredentialsAsList = config.get("paapi_credentials");
+    this.paapiScheduler = new ToadScheduler();
   }
 
-  private balanceAsinsInTasks(rates: number[]): number[] {
+  createTasks(asins: string[]) {
+    const queue = new Queue("parse-asins");
+    const tempChunkedAsins = this.balanceAsinsInTasks(asins);
+    this.tasks = tempChunkedAsins.map((asins, index) => {
+      return new Task(
+        asins,
+        this.paapiCredentialsAsList[index].delay,
+        new Paapi(this.paapiCredentialsAsList[index]),
+        queue
+      );
+    });
+  }
+
+  startTasks() {
+    this.tasks.forEach((task) => this.paapiScheduler.addSimpleIntervalJob(task.getJob()));
+  }
+
+  private balanceRemainingAsinsInTasks(rates: number[]): number[] {
     // Balance schedulers with appropiate number of asin, lower the delay faster the scheduler, faster schedulers receive more asins
     let sumOfRates = rates.reduce((acc, delay) => (acc += delay), 0);
     const schedulersWeights = rates.map((rateo) => rateo / sumOfRates);
     return schedulersWeights;
   }
 
-  createTasks(asins: string[]) {
+  private balanceAsinsInTasks(asins: string[]): string[][] {
     const tempChunkedAsins: string[][] = [];
-    let paapiRates = this.paapiCredentialsAsList.map(
-      (credentials: PaapiCredentials) => 1000 / credentials.delay
-    );
+    let paapiRates = this.paapiCredentialsAsList.map((credentials: PaapiCredentials) => 1000 / credentials.delay);
     let engineWeights: number[];
 
     // Chunk items to match engine weights
     while (asins.length > 0) {
-      engineWeights = this.balanceAsinsInTasks(paapiRates);
+      engineWeights = this.balanceRemainingAsinsInTasks(paapiRates);
       let stopIndex = Math.ceil(asins.length * engineWeights[0]);
       tempChunkedAsins.push(asins.slice(0, stopIndex));
       asins = asins.slice(stopIndex);
       paapiRates = paapiRates.slice(1);
     }
 
-    const paapiScheduler = new ToadScheduler();
-    this.tasks = tempChunkedAsins.map((asins, index) => {
-      return new Task(
-        asins,
-        this.paapiCredentialsAsList[index].delay,
-        new Paapi(this.paapiCredentialsAsList[index]),
-        paapiScheduler
-      );
-    });
-  }
-
-  startTasks() {
-    this.tasks.forEach((task) => {
-      task.start();
-    });
+    return tempChunkedAsins;
   }
 }
