2c7800e3353ffb20a46313fa24a02f0c66972298
Alberto Zanotti <albizan@hotmail.it>
2021-02-26 20:34:06 +0100

    add percentageDiff

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,11 @@
 export const chunk = (input, size: number): any[][] => {
   return input.reduce((arr, item, idx) => {
-    return idx % size === 0 ? [...arr, [item]] : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
+    return idx % size === 0
+      ? [...arr, [item]]
+      : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
   }, []);
+};
+
+export const percentageDiff = (a: number, b: number) => {
+  return ((b - a) * 100) / b;
 };
