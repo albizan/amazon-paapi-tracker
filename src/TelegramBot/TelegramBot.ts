a97ea984235621f2b66b768e6f30ded010e6473b
Alberto Zanotti <albizan@hotmail.it>
2021-03-05 21:32:41 +0100

    Add sendMessage

--- src/TelegramBot/TelegramBot.ts
+++ src/TelegramBot/TelegramBot.ts
@@ -1,18 +1,34 @@
 import { Telegraf } from "telegraf";
 import TaskManager from "../TaskManager";
 import * as config from "config";
+import { Commands } from "./Commands";
+import { startupMessage } from "./types";
 
 const token = config.get("bot.token");
 const logChannel = config.get("bot.log_channel_id");
 
 export default class TelegramBot {
   private instance: Telegraf;
-  constructor(private taskManager: TaskManager) {
+  private commands: Commands;
+  constructor(taskManager: TaskManager) {
     this.instance = new Telegraf(token);
+    this.commands = new Commands(taskManager);
+
+    this.setup();
+  }
+
+  private setup() {
+    this.instance.command("status", this.commands.status);
   }
 
   launch() {
     this.instance.launch();
-    this.instance.telegram.sendMessage(logChannel, `Bot avviato`);
+    // this.instance.telegram.sendMessage(logChannel, startupMessage);
+  }
+
+  sendMessage(msg: string) {
+    this.instance.telegram.sendMessage(logChannel, msg, {
+      parse_mode: "HTML",
+    });
   }
 }
