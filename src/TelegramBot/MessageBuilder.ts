daf133040ab832aeb6b1df6567fb43853a7bcdd1
Alberto Zanotti <albizan@hotmail.it>
2021-03-06 14:30:52 +0100

    Show diff with two decimals

--- src/TelegramBot/MessageBuilder.ts
+++ src/TelegramBot/MessageBuilder.ts
@@ -1,5 +1,7 @@
 import { AmazonProduct } from "../entities/AmazonProduct";
 
 export const defaultMessage = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, diff: number = 0) => {
-  return `<b>${savedItem.title}</b>\nCondizione: ${condition}\nPrezzo: ${oldPrice} => ${price}\n${diff ? `<b>Sconto: ${diff}</b>` : ""}`;
+  return `<a href="${savedItem.url}">${savedItem.title}</a>\nCondizione: ${condition}\nPrezzo: ${oldPrice} => ${price}\n${
+    diff ? `<b>Sconto: ${diff.toFixed(2)}</b>` : ""
+  }`;
 };
