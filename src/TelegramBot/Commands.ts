4239f3b5abbc5daa1c3bfe87a215bc7a2c2646b6
Alberto Zanotti <albizan@hotmail.it>
2021-03-08 22:26:40 +0100

    Add latestRequestedAsin and errors command

--- src/TelegramBot/Commands.ts
+++ src/TelegramBot/Commands.ts
@@ -2,7 +2,7 @@ import { Context } from "telegraf";
 import TaskManager from "../TaskManager";
 
 export class Commands {
-  taskManager: TaskManager;
+  private taskManager: TaskManager;
   constructor(taskManager: TaskManager) {
     this.taskManager = taskManager;
   }
@@ -12,10 +12,24 @@ export class Commands {
     const status = this.taskManager.getStatus();
     let msg = "";
     status.forEach((s) => {
-      msg += `<b>Tag:</b> ${s.tag}\n<b>Avanzamento:</b> ${((100 * s.index) / s.chunks).toFixed(2)}%\n<b>Fine ultima iterazione:</b> ${
-        s.latestIteration || "N/A"
-      }\n\n`;
+      msg += `<b>Tag:</b> ${s.tag}\n<b>Avanzamento:</b> ${((100 * s.index) / s.chunks).toFixed(2)}%\n<b>Ultimo ASIN:</b> <code>${
+        s.latestRequestedAsin
+      }</code>\n<b>Fine ultima iterazione:</b> ${s.latestIteration || "N/A"}\n\n`;
     });
     ctx.replyWithHTML(msg);
+  };
+
+  errors = (ctx: Context) => {
+    console.log("EEEEE");
+    const errors = this.taskManager.getErrors();
+    let msg = "";
+    errors.forEach((errorsArray) => {
+      if (errorsArray.length > 0) {
+        msg = msg + errorsArray.join(",");
+      }
+    });
+    if (msg.length > 0) {
+      ctx.replyWithHTML(msg);
+    } else ctx.reply("Nessun errore riscontrato");
   };
 }
