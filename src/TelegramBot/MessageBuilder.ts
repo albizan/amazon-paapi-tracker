4c2d8db8ef11c2eef014fc0df5112f1136a4641c
Alberto Zanotti <albizan@hotmail.it>
2021-03-10 21:18:21 +0100

    Add seller name

--- src/TelegramBot/MessageBuilder.ts
+++ src/TelegramBot/MessageBuilder.ts
@@ -1,17 +1,17 @@
 import { AmazonProduct } from "../entities/AmazonProduct";
 
-export const defaultMessage = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, diff: number = 0) => {
-  return `<a href="${savedItem.url}">${savedItem.title}</a>\nCondizione: ${condition}\nPrezzo: ${oldPrice} => ${price}\n${
+export const defaultMessage = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, diff: number = 0, seller = null) => {
+  return `<a href="${savedItem.url}">${savedItem.title}</a>\n\n\nCondizione: ${condition}\nPrezzo: ${oldPrice}€ => ${price}€\n${
     diff ? `<b>Sconto: ${diff.toFixed(2)}</b>` : ""
-  }`;
+  }\n${seller ? `<b>Seller: ${seller}</b>` : ""}`;
 };
 
 export const amazonProductInfoMessage = (item: AmazonProduct): string => {
   return `
-    <a href="${item.url}">${item.title}</a>
-    \nAsin: ${item.asin}
-    \nNuovo: ${item.price}
-    \nUsato: ${item.warehousePrice}
+    <a href="${item.url}">${item.title}</a>\n
+    \nAsin: <code>${item.asin}</code>
+    \nNuovo: <i>${item.price}€</i>
+    \nUsato: ${item.warehousePrice || "N/A"}
     \nVisite: ${item.iterations || 0}
     \nUltima visita: ${item.visitedAt || "N/A"}`;
 };
