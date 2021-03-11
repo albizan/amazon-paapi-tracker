21954492a050dfb4544bc1d23ae31028eb710df7
Alberto Zanotti <albizan@hotmail.it>
2021-03-11 22:33:33 +0100

    Do not compare price on first visit

--- src/WorkerManager/WorkerManager.ts
+++ src/WorkerManager/WorkerManager.ts
@@ -33,34 +33,35 @@ class WorkerManager {
     const savedItem = await amazonProductRepository.findOne(amazonRawItem.ASIN);
     let price: number, warehousePrice: number;
 
-    if (savedItem.iterations != 0) {
-      const listingPrice = amazonRawItem.Offers?.Listings[0].Price?.Amount;
-      const seller = amazonRawItem.Offers?.Listings[0].MerchantInfo?.Name;
-      const summaries: Summary[] = amazonRawItem.Offers?.Summaries || [];
+    const listingPrice = amazonRawItem.Offers?.Listings[0].Price?.Amount;
+    const seller = amazonRawItem.Offers?.Listings[0].MerchantInfo?.Name;
+    const summaries: Summary[] = amazonRawItem.Offers?.Summaries || [];
 
-      if (listingPrice) {
-        this.comparePrice(savedItem, listingPrice, savedItem.price, "New", seller);
-        price = listingPrice;
-      }
-      const usedSummary = summaries.find((summary) => summary.Condition?.Value === "Used");
-      if (usedSummary) {
-        this.comparePrice(savedItem, usedSummary.LowestPrice.Amount, savedItem.warehousePrice, "Used");
-        warehousePrice = usedSummary.LowestPrice.Amount;
-      }
-
-      /* summaries.forEach((summary) => {
-        if (summary.Condition?.Value === "New") {
-          console.log(`Prezzo: ${listingPrice} (${summary.LowestPrice.Amount}) -> Seller: ${seller}`);
-          this.comparePrice(savedItem, summary.LowestPrice.Amount, savedItem.price, "New");
-          price = summary.LowestPrice.Amount;
-        } else if (summary.Condition?.Value === "Used") {
-          this.comparePrice(savedItem, summary.LowestPrice.Amount, savedItem.warehousePrice, "Used");
-          warehousePrice = summary.LowestPrice.Amount;
-        }
-      }); */
-    } else {
+    if (savedItem.iterations === 0) {
       isNewProduct = true;
     }
+
+    if (listingPrice) {
+      this.comparePrice(isNewProduct, savedItem, listingPrice, savedItem.price, "New", seller);
+      price = listingPrice;
+    }
+    const usedSummary = summaries.find((summary) => summary.Condition?.Value === "Used");
+    if (usedSummary) {
+      this.comparePrice(isNewProduct, savedItem, usedSummary.LowestPrice.Amount, savedItem.warehousePrice, "Used");
+      warehousePrice = usedSummary.LowestPrice.Amount;
+    }
+
+    /* summaries.forEach((summary) => {
+          if (summary.Condition?.Value === "New") {
+            console.log(`Prezzo: ${listingPrice} (${summary.LowestPrice.Amount}) -> Seller: ${seller}`);
+            this.comparePrice(savedItem, summary.LowestPrice.Amount, savedItem.price, "New");
+            price = summary.LowestPrice.Amount;
+          } else if (summary.Condition?.Value === "Used") {
+            this.comparePrice(savedItem, summary.LowestPrice.Amount, savedItem.warehousePrice, "Used");
+            warehousePrice = summary.LowestPrice.Amount;
+          }
+        }); */
+
     this.updateAmazonProduct(amazonRawItem, savedItem, isNewProduct, price, warehousePrice);
   };
 
@@ -69,6 +70,7 @@ class WorkerManager {
       savedItem.title = amazonRawItem.ItemInfo?.Title?.DisplayValue;
       savedItem.url = amazonRawItem.DetailPageURL;
       savedItem.image = amazonRawItem.Images?.Primary?.Large.URL;
+      console.log("New entry: " + savedItem.title);
     }
     savedItem.price = price;
     savedItem.warehousePrice = warehousePrice;
@@ -77,7 +79,10 @@ class WorkerManager {
     amazonProductRepository.save(savedItem);
   };
 
-  comparePrice = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, sellerName?: string) => {
+  comparePrice = (isnewProduct: boolean, savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, sellerName?: string) => {
+    if (isnewProduct) {
+      return;
+    }
     // Product is available again
     if (!oldPrice) {
       try {
