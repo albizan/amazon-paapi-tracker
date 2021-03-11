1a6e4d6156ea5631d0ca3ad14dbd508590b44700
Alberto Zanotti <albizan@hotmail.it>
2021-03-11 20:41:04 +0100

    Differentiate message types

--- src/TelegramBot/MessageBuilder.ts
+++ src/TelegramBot/MessageBuilder.ts
@@ -1,9 +1,23 @@
 import { AmazonProduct } from "../entities/AmazonProduct";
 
-export const defaultMessage = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, diff: number = 0, seller = null) => {
-  return `<a href="${savedItem.url}">${savedItem.title}</a>\n\n\nCondizione: ${condition}\nPrezzo: ${oldPrice}€ => ${price}€\n${
-    diff ? `<b>Sconto: ${diff.toFixed(2)}</b>` : ""
-  }\n${seller ? `<b>Seller: ${seller}</b>` : ""}`;
+export const discountMessage = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, diff: number = 0, seller = "N/A") => {
+  return `
+    <b>ABBASSAMENTO DI PREZZO</b>\n
+    <a href="${savedItem.image}">${savedItem.title}</a>\n
+    \nAsin: <code>${savedItem.asin}</code>
+    \nCondizione: ${condition}
+    \nPrezzo: ${oldPrice}€ => ${price}€
+    \nSconto: ${diff.toFixed(2)}%
+    \nVenditore: ${seller}`;
+};
+
+export const availableAgainMessage = (savedItem: AmazonProduct, price: number, condition: string, seller = "N/A") => {
+  return `
+    <b>NUOVAMENTE DISPONIBILE</b>\n
+    <a href="${savedItem.image}">${savedItem.title}</a>\n
+    \nAsin: <code>${savedItem.asin}</code>
+    \nCondizione: ${condition}
+    \nVenditore: ${seller}`;
 };
 
 export const amazonProductInfoMessage = (item: AmazonProduct): string => {
