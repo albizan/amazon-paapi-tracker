a08670f3849525ff5cc7245d3b3c01476e13e6d3
Alberto Zanotti <albizan@hotmail.it>
2021-03-12 11:46:17 +0100

    Add image url in hidden anchor

--- src/TelegramBot/MessageBuilder.ts
+++ src/TelegramBot/MessageBuilder.ts
@@ -2,8 +2,9 @@ import { AmazonProduct } from "../entities/AmazonProduct";
 
 export const discountMessage = (savedItem: AmazonProduct, price: number, oldPrice: number, condition: string, diff: number = 0, seller = "N/A") => {
   return `
+    <a href='${savedItem.image}'>&#8204;</a>
     <b>ABBASSAMENTO DI PREZZO</b>\n
-    \n<a href="${savedItem.image}">${savedItem.title}</a>
+    \n${savedItem.title}
     \nAsin: <code>${savedItem.asin}</code>
     \nCondizione: ${condition}
     \nPrezzo: <i>${oldPrice}€ => ${price}€</i>
@@ -14,8 +15,9 @@ export const discountMessage = (savedItem: AmazonProduct, price: number, oldPric
 
 export const availableAgainMessage = (savedItem: AmazonProduct, price: number, condition: string, seller = "N/A") => {
   return `
+    <a href='${savedItem.image}'>&#8204;</a>
     <b>NUOVAMENTE DISPONIBILE</b>\n
-    \n<a href="${savedItem.image}">${savedItem.title}</a>
+    \n${savedItem.title}
     \nAsin: <code>${savedItem.asin}</code>
     \nCondizione: ${condition}
     \nPrezzo: <i>${price}€</i>
@@ -27,8 +29,9 @@ export const amazonProductInfoMessage = (item: AmazonProduct): string => {
   const price = item.price ? item.price + "€" : "N/A";
   const warehousePrice = item.warehousePrice ? item.warehousePrice + "€" : "N/A";
   return `
+    <a href='${item.image}'>&#8204;</a>
     <b>INFORMAZIONI PRODOTTO</b>\n
-    \n<a href="${item.image}">${item.title}</a>
+    \n${item.title}
     \nAsin: <code>${item.asin}</code>
     \nPrezzo: <i>${price}</i>
     \nPrezzo usato: <i>${warehousePrice}</i>
