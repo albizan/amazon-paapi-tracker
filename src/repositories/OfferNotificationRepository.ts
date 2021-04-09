3aac5b5ba4754756fdfd3ed9e95b64c4ffb24b6e
Alberto Zanotti <albizan@hotmail.it>
2021-04-09 11:00:40 +0200

    Add repo

---
+++ src/repositories/OfferNotificationRepository.ts
@@ -0,0 +1,23 @@
+import { getRepository } from "typeorm";
+import { AmazonProduct } from "../entities/AmazonProduct";
+import { OfferNotification } from "../entities/OfferNotification";
+
+class OfferNotificationRepository {
+  async save(offerNotification: OfferNotification) {
+    try {
+      const offerNotificationRepository = getRepository(OfferNotification);
+      console.log(offerNotification);
+      await offerNotificationRepository.save(offerNotification);
+    } catch (error) {
+      console.error(error);
+      console.error("Error saving offer notification on Database");
+    }
+  }
+
+  async findOne(id: string) {
+    const offerNotificationRepository = getRepository(OfferNotification);
+    return await offerNotificationRepository.findOne(id);
+  }
+}
+
+export default new OfferNotificationRepository();
