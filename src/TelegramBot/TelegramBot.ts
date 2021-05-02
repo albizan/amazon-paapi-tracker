646a73a317ea14301a1240008171876b3215e7be
ALberto Zanotti <albizan@hotmail.it>
2021-05-02 15:28:16 +0200

    Retrieve seller name on "Used" offers

--- src/TelegramBot/TelegramBot.ts
+++ src/TelegramBot/TelegramBot.ts
@@ -5,6 +5,11 @@ import { setupMiddlewares } from "./Middleware";
 import amazonProductRepository from "../repositories/AmazonProductRepository";
 import offerNotificationRepository from "../repositories/OfferNotificationRepository";
 import { amazonProductInfoMessage, channelNotification } from "../TelegramBot/MessageBuilder";
+import { APPROVE, NEW, USED } from "../types";
+import * as config from "config";
+import PaapiCredentials from "../PaapiCredentials";
+import Paapi from "../apis/amazon";
+import { Item } from "paapi5-typescript-sdk";
 
 const token = process.env.BOT_TOKEN;
 const logChannel = process.env.BOT_OUT_CHANNEL;
@@ -44,16 +49,34 @@ export default class TelegramBot {
     this.instance.on("callback_query", async (ctx: any) => {
       // Using context shortcut
       ctx.answerCbQuery();
-      if (ctx.callbackQuery.data == "Approva") {
+
+      if (ctx.callbackQuery.data == APPROVE) {
         const messageId = ctx.callbackQuery.message.message_id;
         const offer = await offerNotificationRepository.findOne(messageId);
-        // console.log(notification);
-        this.instance.telegram.sendMessage(offerChannel, channelNotification(offer.product, offer.price, offer.sellerName, offer.type), {
-          parse_mode: "HTML",
-          reply_markup: {
-            inline_keyboard: [[Markup.button.url("Apri Amazon", offer.product.url)]],
-          },
-        });
+
+        if (offer?.type == NEW) {
+          this.instance.telegram.sendMessage(offerChannel, channelNotification(offer.product, offer.price, offer.sellerName, offer.type), {
+            parse_mode: "HTML",
+            reply_markup: {
+              inline_keyboard: [[Markup.button.url("Apri Amazon", offer.product.url)]],
+            },
+          });
+        } else if (offer?.type == USED) {
+          const item = await this.getSingleProductInfoFromAmazon(offer.product.asin);
+          if (item) {
+            if (item.Offers?.Listings[0]?.Price?.Amount === offer.price) {
+              const sellerName = item.Offers?.Listings[0]?.MerchantInfo?.Name;
+              this.instance.telegram.sendMessage(offerChannel, channelNotification(offer.product, offer.price, sellerName, offer.type), {
+                parse_mode: "HTML",
+                reply_markup: {
+                  inline_keyboard: [[Markup.button.url("Apri Amazon", offer.product.url)]],
+                },
+              });
+            }
+          } else {
+            ctx.reply("Non ho trovato corrispondenze con il prezzo precedentemente inviato, impossibile inoltrare messaggio nel canale");
+          }
+        }
       }
     });
   }
@@ -71,5 +94,12 @@ export default class TelegramBot {
 
   getInstance(): Telegraf {
     return this.instance;
+  }
+
+  async getSingleProductInfoFromAmazon(asin: string): Promise<Item> {
+    const credentials: PaapiCredentials = config.get("paapi_credentials")[0];
+    const tempPaapi = new Paapi(credentials);
+    const data = await tempPaapi.getItems([asin], USED);
+    return data?.ItemsResult?.Items[0];
   }
 }
