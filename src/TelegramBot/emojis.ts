e14d8239c4904f32652a05dbed82639f0a8963e3
ALberto Zanotti <albizan@hotmail.it>
2021-05-06 18:43:59 +0200

    Add emoji codes

---
+++ src/TelegramBot/emojis.ts
@@ -0,0 +1,13 @@
+export default {
+  packageIcon: "\u{1F4E6}",
+  key: "\u{1F511}",
+  exclamationMark: "\u{2757}",
+  link: "\u{1F517}",
+  truck: "\u{1F69A}",
+  trophy: "\u{1F3C6}",
+  silhouette: "\u{1F464}",
+  creditCard: "\u{1F4B3}",
+  banknote: "\u{1F4B5}",
+  hourGlass: "\u{23F3}",
+  chart: "\u{1F4C8}",
+};
